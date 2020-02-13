220c00157df9722629b10e6be9a0786819bfa086
ChenRuixiang1997 <631671241@qq.com>
2020-02-13 17:35:56 +0800

    First spring-boot web demo

---
+++ src/main/java/com/self/learning/SpringbootWebDemo1Application.java
@@ -0,0 +1,13 @@
+package com.self.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/self/learning/SpringbootWebDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
